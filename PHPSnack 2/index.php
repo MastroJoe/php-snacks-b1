c21250699fa5f5363ac8beb52b4196706194a736
Giorgio <giorgio.pignattari@gmail.it>
2021-04-29 16:05:00 +0200

    snack2 finito, inizio snack3

--- PHPSnack 2/index.php
+++ PHPSnack 2/index.php
@@ -4,5 +4,23 @@ che mail contenga un punto e una chiocciola e che age sia un numero.
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <?php
-  
+  // creo le variabili con GET
+  $nome = $_GET['name'];
+  $email = $_GET['email'];
+  $eta = $_GET['age'];
+
+  var_dump($nome, $email, $eta);
+
+  // se le condizioni sono rispettate
+  if ((strlen($nome) > 3) &&
+      filter_var($email, FILTER_VALIDATE_EMAIL) &&
+      filter_var($eta, FILTER_VALIDATE_INT)) {
 ?>
+    <p style="font-size: 25px; color: green;">
+      Accesso riuscito
+    </p>
+<?php } else { ?>
+    <p style="font-size: 25px; color: tomato;">
+      Accesso negato
+    </p>
+<?php } ?>
